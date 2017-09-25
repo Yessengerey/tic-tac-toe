9ab7d533cffb095070e35fea2e12922e2caadafe
Gary <yes.murz@gmail.com>
2017-09-25 15:34:49 -0700

    Create initial loop

--- index.js
+++ index.js
@@ -17,9 +17,45 @@ class Game {
   constructor(players) {
     this.numOfPlayers = players;
   }
+};
+
+class GameBoard {
+  constructor(/* size of the board */) {
+
+  }
 }
 
 var GameLogic = function() {
-  var theGame = new Game(2);
+  var winner = false;
+  var theGame = {};
   console.log('Welcome to Command Line Tic Tac Toe!');
+
+  rl.question('Start the game? ', (answer) => {
+    // while (winner === false) {
+      if (answer !== 'yes') {
+        console.log('Good Bye!');
+        rl.close();
+      } else {
+        theGame = new Game();
+        showBoard();
+        makeAMove();
+        // rl.close();
+      }
+    // }
+  });
 }();
+
+var showBoard = function() {
+  console.log('Your board:\n');
+  for (var i = 0; i < board.length; i++) {
+    console.log(board[i] + '\n');
+  }
+};
+
+var makeAMove = function() {
+  rl.question('Your next move? (e.g. 1,2) ', (answer) => {
+    var move = answer.split(',');
+    board[move[0]][move[1]] = 'X';
+    showBoard();
+  });
+};
